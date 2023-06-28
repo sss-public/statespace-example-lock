6864832312b86a9457f80b6afc469757e3f0ab9a
Suhabe Bugrara <suhabe@bugrara.com>
2023-06-27 18:03:11 -0700

    set deterministic time

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,16 @@ import "@statespace/hardhat-testgen"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      initialDate: new Date(0).toISOString(), // 1970-01-01T00:00:00.000Z
+    }
+  },
+  testgen: {
+    settings: {
+      deterministic: ["time"],
+    }
+  }
 };
 
 export default config;
